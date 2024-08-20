3d9f7dd1feb1f4ae4690940c4fceacc43dd5b520
Srihari Nair <sriharinair134@gmail.com>
2024-08-21 02:50:33 +0530

    Add files via upload

---
+++ 9] 1 C++ Program To Illustrate Pointers.cpp
@@ -0,0 +1,20 @@
+//Name: Srihari Nair
+//Prn: 23070123131
+//Class: EnTC B-2
+#include<iostream>
+using namespace std;
+void geeks()
+{
+    int var = 70;
+    int* ptr;
+    ptr = &var;
+
+    cout<<"Value at ptr = " << ptr << "\n";
+    cout<<"Value at var = " << *ptr <<"\n";
+    cout<<"Value at *ptr = " << *ptr <<"\n";
+}
+int main()
+{
+    geeks();
+    return 0;
+}
